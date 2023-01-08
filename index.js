75f7c28cc07a5154191a3a18dddf765743617e93
Brian Hunley <brian.hunley@gmail.com>
2023-01-08 09:38:17 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+console.log(`
+C. Brian Hunley
+Software Engineer
+
+A diverse background in Information Technology, Equipment Manufacturing and Service, and New Innovative Equipment Engineering provides a unique perspective for creative web applications. My focus is on developing web applications using React.js, Redux Toolkit, Node.js, Node.js Libraries, Material-UI, Express.js, MongoDB and PostgreSQL.
+
+LinkedIn: https://www.linkedin.com/in/brian-hunley/
+GitHub:   https://github.com/brianhunley
+
+`);
